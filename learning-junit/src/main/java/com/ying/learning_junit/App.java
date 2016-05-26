1ee2b35173f3a4240a4dee5c035f2b4de32356f5
Scott <sco.suen.lab@gmail.com>
2016-05-25 20:31:45 -0400

    Feat: Initialize project.

---
+++ learning-junit/src/main/java/com/ying/learning_junit/App.java
@@ -0,0 +1,13 @@
+package com.ying.learning_junit;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfiguration.class);
+	}
+}
---
+++ learning-junit/src/main/java/com/ying/learning_junit/SpringConfiguration.java
@@ -0,0 +1,8 @@
+package com.ying.learning_junit;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfiguration {
+
+}
